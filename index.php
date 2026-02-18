ad05dcfda452d57729879f6942269451b85994a1
boristotal <boris@totalground.com>
2026-02-18 12:35:33 -0600

    no funciona

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 require 'vendor/autoload.php';
 
 $app = new Slim\App();
-$app->get('/', function ($request, $response, $args) {
-    return $response->write("Hello, World!");
-});
 
 $app->post('/data', function ($request, $response, $args) {
-    $data = $request->getParsedBody();
-    return $response->withJson($data);
+    //$data = $request->getParsedBody();
+    //return $response->withJson($data);
+    return $response->write("So ? ? ");
+});
+
+$app->get('/', function ($request, $response, $args) {
+    return $response->write("Aquel amor ? ");
 });
 
 $app->run();
-
-
 ?>
